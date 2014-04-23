cd70cca2354aef8f762790ca7282c36f95e5a69d
Robin Böhm <robinboehm@googlemail.com>
2014-04-23 17:08:16 +0200

    add some comments for the talk

--- src/scripts/directives/simpleDirective.js
+++ src/scripts/directives/simpleDirective.js
@@ -4,6 +4,7 @@ angular.module('meetup-example')
   .directive('simple', function () {
     return {
       restrict: 'E',
+      //template: '<h1>Hello World!</h1>'
       templateUrl: 'views/simple.tpl.html'
     }
   });
